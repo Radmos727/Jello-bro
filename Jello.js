e9395946ce128a4d60989a81898e4e461b94beb9
Radmos <sandrexaa6969@gmail.com>
2024-03-02 14:47:13 +0400

    removed dumb stuff

--- Jello.js
+++ Jello.js
@@ -36,45 +36,13 @@ client.on('messageCreate', (message) => {
     }
 });
 
+
+
 // TODO: dont kms
 
 // lore and uhhhh retard lisekuu idk
 
-// function sendRandomMessage() {
-//     const specialResponseChance = 0.5;
-//     const specialResponse = "https://media.discordapp.net/attachments/1169451854820802704/1205662088429830244/caption.gif?ex=65e269d2&is=65cff4d2&hm=8b4329dfaffca920d618ad679fa98c4c62377513e35f964a651eceee72d61907&";
 
-//     if (Math.random() < specialResponseChance) {
-//         let mostActiveChannel = findMostActiveChannel();
-
-//         if (mostActiveChannel) {
-//             mostActiveChannel.send(specialResponse);
-//         }
-//     }
-// }
-
-
-// function findMostActiveChannel() {
-//     let mostActive = null;
-//     let highestCount = 0;
-
-//     client.channels.cache.forEach(channel => {
-//         if (channel.type === 'GUILD_TEXT' && channel.messages.cache.size >= messageCountThreshold) {
-//             let count = channel.messages.cache.filter(message => message.createdTimestamp > Date.now() - checkInterval).size;
-
-//             console.log(`Channel: ${channel.name}, Count: ${count}`); // Log channel details for debugging
-
-//             if (count > highestCount) {
-//                 highestCount = count;
-//                 mostActive = channel;
-//             }
-//         }
-//     });
-
-//     console.log(`Most Active Channel: ${mostActive ? mostActive.name : 'None'}`); // Log the mostActive channel for debugging
-
-//     return mostActive;
-// }
 
 
 client.login(process.env.DISCORD_TOKEN);
